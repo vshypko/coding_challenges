9a467488ff3d7c9d1c206e22cc7a96c03f3f519f
vitali <vshypko@gmail.com>
2018-10-03 22:24:03 -0700

    test

--- problems/leetcode/fizz_buzz.py
+++ problems/leetcode/fizz_buzz.py
@@ -5,6 +5,7 @@ class Solution:
         :rtype: List[str]
         """
         result = list()
+
         for i in range(1, n+1):
             if i % 3 == 0 and i % 5 == 0:
                 result.append("FizzBuzz")
