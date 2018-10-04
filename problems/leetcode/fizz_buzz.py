ca4b38bba0a1c2e82c09925a28c94076c62ce44c
vitali <vshypko@gmail.com>
2018-10-03 22:22:53 -0700

    start leetcode uploads. fizzbuzz

---
+++ problems/leetcode/fizz_buzz.py
@@ -0,0 +1,18 @@
+class Solution:
+    def fizz_buzz(self, n):
+        """
+        :type n: int
+        :rtype: List[str]
+        """
+        result = list()
+        for i in range(1, n+1):
+            if i % 3 == 0 and i % 5 == 0:
+                result.append("FizzBuzz")
+            elif i % 3 == 0:
+                result.append("Fizz")
+            elif i % 5 == 0:
+                result.append("Buzz")
+            else:
+                result.append(str(i))
+
+        return result
