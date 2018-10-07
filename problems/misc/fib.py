ddf298171462b5c89bcefd51574e3a3dafe80e79
vitali <vshypko@gmail.com>
2018-10-07 02:39:36 -0700

    add memoized fib

--- problems/misc/fib.py
+++ problems/misc/fib.py
@@ -1,8 +1,31 @@
+import time
+
+
 def nthfib(n):
-    if n <= 1:
+    if n <= 0:
+        return
+    if n == 1 or n == 2:
         return 1
     return nthfib(n-1) + nthfib(n-2)
 
-print(nthfib(1))
-print(nthfib(5))
-print(nthfib(10))
+
+def memoizedNthfib(n, mem=list()):
+    if not mem:
+        mem = [0] * (n + 1)
+    if mem and mem[n]:
+        return mem[n]
+    if n == 1 or n == 2:
+        result = 1
+    else:
+        result = memoizedNthfib(n-1, mem) + memoizedNthfib(n-2, mem)
+    mem[n] = result
+    return result
+
+
+n = 33
+start = time.time()
+print("Regular fib:", nthfib(n))
+print("Took %f seconds.\n" % (time.time() - start))
+start = time.time()
+print("Memoized fib:", memoizedNthfib(n))
+print("Took %f seconds." % (time.time() - start))
