811c059a112d1db7720d09266e09ee1db769e8a8
vitali <vshypko@gmail.com>
2018-10-16 09:19:14 -0700

    add reverseVowels

---
+++ problems/leetcode/reverseVowels.py
@@ -0,0 +1,17 @@
+class Solution:
+    def reverseVowels(self, s):
+        """
+        :type s: str
+        :rtype: str
+        """
+        vowels = ['a', 'e', 'i', 'o', 'u']
+        index = []
+        seen = []
+        word = list(s)
+        for i in range(len(s)):
+            if word[i].lower() in vowels:
+                index.append(i)
+                seen.append(word[i])
+        for i in range(len(seen)):
+            word[index[i]] = seen[len(seen) - i - 1]
+        return ''.join(word)
