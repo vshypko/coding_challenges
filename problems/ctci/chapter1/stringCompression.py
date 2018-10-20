d81ffb92dd804e068abdc9e985822fba1a8e35e1
vitali <vshypko@gmail.com>
2018-10-20 15:13:48 -0700

    setup for stringCompression

---
+++ problems/ctci/chapter1/stringCompression.py
@@ -0,0 +1,14 @@
+# 1.6
+class Solution:
+    def __init__(self):
+        # for testing purposes
+        pass
+
+    # O() runtime
+    # O() space
+    def stringCompression(self, s):
+        return s
+
+
+assert (Solution().stringCompression("aabccccaaa") == "a2b1c5a3")
+assert (Solution().stringCompression("abcd") == "abcd")
