ad511034c3f5322ecb7e799565963902540f7f71
vitali <vshypko@gmail.com>
2018-10-19 22:58:03 -0700

    add n log n solution to 1.1

--- problems/ctci/chapter1/isUnique.py
+++ problems/ctci/chapter1/isUnique.py
@@ -1,5 +1,7 @@
 # 1.1
 class Solution:
+    # O(N) runtime
+    # O(N) space
     def isUnique(s):
         if not s:
             return True
@@ -12,4 +14,20 @@ class Solution:
                 return False
         return True
 
-    assert isUnique("abcd")
+    # O(N logN) runtime
+    # O(1) space
+    def isUniqueSort(s):
+        if not s:
+            return True
+
+        sortedS = sorted(s)
+        for i in range(len(sortedS) - 1):
+            if sortedS[i] == sortedS[i + 1]:
+                return False
+        return True
+
+    assert (isUnique("abcd") is True)
+    assert (isUnique("dzgqmzksqod") is False)
+
+    assert (isUniqueSort("abcd") is True)
+    assert (isUniqueSort("dzgqmzksqod") is False)
