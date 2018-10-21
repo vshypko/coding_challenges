e1797713631e6e08cfc34d374129796ee4d251a7
vitali <vshypko@gmail.com>
2018-10-21 16:54:27 -0700

    add stringCompression

--- problems/ctci/chapter1/stringCompression.py
+++ problems/ctci/chapter1/stringCompression.py
@@ -7,8 +7,21 @@ class Solution:
     # O() runtime
     # O() space
     def stringCompression(self, s):
-        return s
+        if len(s) <= 0:
+            return s
+
+        compressedString = ""
+        counter = 1
+
+        for i in range(len(s) - 1):
+            if s[i] == s[i+1]:
+                counter += 1
+            if s[i] != s[i+1] or i == len(s) - 2:
+                compressedString += s[i] + str(counter)
+                counter = 1
+
+        return compressedString if len(compressedString) < len(s) else s
 
 
-assert (Solution().stringCompression("aabccccaaa") == "a2b1c5a3")
+assert (Solution().stringCompression("aabcccccaaa") == "a2b1c5a3")
 assert (Solution().stringCompression("abcd") == "abcd")
