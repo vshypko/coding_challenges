9ee0b9e6788c1b8a60d55b9a2b06d9d69efb8323
vitali <vshypko@gmail.com>
2018-10-21 16:56:55 -0700

    setup rotateMatrix

---
+++ problems/ctci/chapter1/rotateMatrix.py
@@ -0,0 +1,13 @@
+# 1.7
+class Solution:
+    def __init__(self):
+        # for testing purposes
+        pass
+
+    # O() runtime
+    # O() space
+    def rotateMatrix(self, m):
+        return None
+
+assert (Solution().rotateMatrix([]) == None)
+assert (Solution().rotateMatrix([]) == None)
