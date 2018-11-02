4b97f3c7e0129955824a962b84de3cefaa721d46
vitali <vshypko@gmail.com>
2018-11-01 23:06:55 -0700

    add containsDuplicate

---
+++ problems/leetcode/containsDuplicate.py
@@ -0,0 +1,8 @@
+# 217
+class Solution:
+    def containsDuplicate(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: bool
+        """
+        return len(set(nums)) != len(nums)
