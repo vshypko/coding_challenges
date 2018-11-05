686d51084fa5e09468528c4fb800431ae8779773
vitali <vshypko@gmail.com>
2018-11-04 16:27:58 -0800

    add intersection

--- problems/ctci/chapter2/intersection.py
+++ problems/ctci/chapter2/intersection.py
@@ -29,22 +29,59 @@ class Solution:
         # for testing purposes
         pass
 
-    # O() runtime
-    # O() space
+    # O(A + b) runtime
+    # O(1) space
     def intersection(self, linkedList1, linkedList2):
         if not linkedList1 or not linkedList2:
             return False
-        pointer1 = self.reverseLinkedList(linkedList1)
-        pointer2 = self.reverseLinkedList(linkedList2)
-        intersectingNode = None
+
+        isIntersection = self.isIntersection(linkedList1, linkedList2)
+        if not isIntersection[0]:
+            return None
+
+        length1 = isIntersection[1]
+        length2 = isIntersection[2]
+
+        i = 0
+        pointer1 = linkedList1
+        pointer2 = linkedList2
+        if length1 > length2:
+            while (i < length1 - length2):
+                pointer1 = pointer1.next
+                i += 1
+        else:
+            while (i < length2 - length1):
+                pointer2 = pointer2.next
+                i += 1
 
         while pointer1 and pointer2:
-            if pointer1 is pointer2:
-                intersectingNode = pointer1
-            pointer1, pointer2 = pointer1.next, pointer2.next
+            if pointer1 == pointer2:
+                return pointer1
+            else:
+                pointer1 = pointer1.next
+                pointer2 = pointer2.next
 
-        print(intersectingNode.val)
-        return intersectingNode
+        return None
+
+
+    def isIntersection(self, linkedList1, linkedList2):
+        if not linkedList1 or not linkedList2:
+            return False
+
+        pointer1 = linkedList1
+        length1 = 0
+        pointer2 = linkedList2
+        length2 = 0
+
+        while pointer1 and pointer1.next:
+            pointer1 = pointer1.next
+            length1 += 1
+
+        while pointer2 and pointer2.next:
+            pointer2 = pointer2.next
+            length2 += 1
+
+        return (pointer1 == pointer2, length1 + 1, length2 + 1)
 
     def reverseLinkedList(self, linkedList):
         prev = pointer = linkedList
@@ -83,4 +120,4 @@ linkedList1.next.next.next.next.next = Node7
 linkedList2 = Node5
 linkedList2.next = Node4
 
-Solution().intersection(linkedList1, linkedList2)
+assert Solution().intersection(linkedList1, linkedList2).val == 4
