c98874a42c84758a6db2ee12c00b7cc7f863466e
vitali <vshypko@gmail.com>
2018-11-05 13:48:51 -0800

    add findKthLargest

---
+++ problems/leetcode/findKthLargest.py
@@ -0,0 +1,20 @@
+# 215
+class Solution:
+    def findKthLargest(self, nums, k):
+        """
+        :type nums: List[int]
+        :type k: int
+        :rtype: int
+        """
+        # O(N logN)
+        # return sorted(nums)[-k]
+
+        # O(N)
+        #         heap = list()
+        #         for num in nums:
+        #             heapq.heappush(heap, num)
+        #             if len(heap) > k:
+        #                 heapq.heappop(heap)
+
+        #         return heapq.heappop(heap)
+        return heapq.nlargest(k, nums)[-1]
