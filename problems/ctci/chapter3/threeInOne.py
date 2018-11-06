b1e682431c49efe4061b606dbdb19e0b360d9144
vitali <vshypko@gmail.com>
2018-11-05 22:22:38 -0800

    add threeInOne

---
+++ problems/ctci/chapter3/threeInOne.py
@@ -0,0 +1,13 @@
+# 3.1
+class Solution:
+    def __init__(self):
+        # for testing purposes
+        pass
+
+    def threeInOne(self, s1, s2):
+        # divide array into 3 parts [0 - n/3), [n/3 - 2n/3), [2n/3 - n)
+        # if we need array sizes to be flexible, need to resize respective parts
+        return
+
+
+assert (Solution().isOneAway("pale", "bake") is False)
