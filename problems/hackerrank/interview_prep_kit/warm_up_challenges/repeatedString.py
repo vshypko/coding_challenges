92c09175ab0f12283efe4d486f2866e701c0e4d1
vitali <vshypko@gmail.com>
2018-11-24 09:59:57 -0800

    add repeatedString

--- problems/hackerrank/interview_prep_kit/warm_up_challenges/repeatedString.py
+++ problems/hackerrank/interview_prep_kit/warm_up_challenges/repeatedString.py
@@ -8,6 +8,12 @@ import sys
 
 # Complete the repeatedString function below.
 def repeatedString(s, n):
+    if n <= len(s):
+        return s.count('a', 0, n)
+
+    counter = s.count('a') * (n // len(s))
+    counter += s[:n % len(s)].count('a')
+    return counter
 
 if __name__ == '__main__':
     fptr = open(os.environ['OUTPUT_PATH'], 'w')
