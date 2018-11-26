bbba213a26951ae059bd42646d3b6e028d7c4e4a
vitali <vshypko@gmail.com>
2018-11-25 16:24:55 -0800

    add hourglassSum

--- problems/hackerrank/interview_prep_kit/2_arrays/hourglassSum.py
+++ problems/hackerrank/interview_prep_kit/2_arrays/hourglassSum.py
@@ -8,7 +8,21 @@ import sys
 
 # Complete the hourglassSum function below.
 def hourglassSum(arr):
-    pass
+    maxSum = float('-inf')
+
+    for i in range(1, len(arr) - 1):
+        for j in range(1, len(arr[0]) - 1):
+            topLeft     = arr[i-1][j-1]
+            top         = arr[i-1][j]
+            topRight    = arr[i-1][j+1]
+            current     = arr[i][j]
+            bottomLeft  = arr[i+1][j-1]
+            bottom      = arr[i+1][j]
+            bottomRight = arr[i+1][j+1]
+            currentSum = topLeft + top + topRight + current + bottomLeft + bottom + bottomRight
+            maxSum = max(maxSum, currentSum)
+
+    return maxSum
 
 if __name__ == '__main__':
     fptr = open(os.environ['OUTPUT_PATH'], 'w')
