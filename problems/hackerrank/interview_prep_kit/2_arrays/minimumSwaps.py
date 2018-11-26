7a9f2484b90f527acdcfe42707505a2548aae4f9
vitali <vshypko@gmail.com>
2018-11-26 11:53:07 -0800

    add minimumSwaps skeleton

---
+++ problems/hackerrank/interview_prep_kit/2_arrays/minimumSwaps.py
@@ -0,0 +1,23 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the minimumSwaps function below.
+def minimumSwaps(arr):
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input())
+
+    arr = list(map(int, input().rstrip().split()))
+
+    res = minimumSwaps(arr)
+
+    fptr.write(str(res) + '\n')
+
+    fptr.close()
