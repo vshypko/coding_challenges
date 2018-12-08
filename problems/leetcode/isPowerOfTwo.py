18aff53c3ffa198a016eb37e7bb730553836636f
vitali <vshypko@gmail.com>
2018-12-07 18:31:15 -0800

    add isPowerOfTwo

---
+++ problems/leetcode/isPowerOfTwo.py
@@ -0,0 +1,16 @@
+# 231
+class Solution:
+    def isPowerOfTwo(self, n):
+        """
+        :type n: int
+        :rtype: bool
+        """
+        if n <= 0:
+            return False
+
+        while n > 1:
+            if n % 2 == 1:
+                return False
+            n = n // 2
+
+        return True
