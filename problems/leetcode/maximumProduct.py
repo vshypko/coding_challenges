e1d27edf671cc3f53cbc4ce0279e77b96bc981ce
vitali <vshypko@gmail.com>
2018-12-08 20:35:02 -0800

    simplify maximumProduct

--- problems/leetcode/maximumProduct.py
+++ problems/leetcode/maximumProduct.py
@@ -6,9 +6,4 @@ class Solution:
         :rtype: int
         """
         nums.sort()
-
-        if len(nums) == 3:
-            return nums[0] * nums[1] * nums[2]
-        elif len(nums) == 4:
-            return max(nums[0] * nums[1] * nums[3], nums[1] * nums[2] * nums[3])
         return max(nums[0] * nums[1] * nums[-1], nums[-3] * nums[-2] * nums[-1])
