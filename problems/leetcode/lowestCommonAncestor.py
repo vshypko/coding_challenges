87c9d8c6a8e2186e387d96b8adceed37b39b0cc5
vitali <vshypko@gmail.com>
2019-01-07 00:46:31 -0800

    add lowestCommonAncestor

---
+++ problems/leetcode/lowestCommonAncestor.py
@@ -0,0 +1,24 @@
+# 235
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution:
+    def lowestCommonAncestor(self, root, p, q):
+        """
+        :type root: TreeNode
+        :type p: TreeNode
+        :type q: TreeNode
+        :rtype: TreeNode
+        """
+        if not root or not p or not q:
+            return None
+
+        if p.val < root.val and q.val < root.val:
+            return self.lowestCommonAncestor(root.left, p, q)
+        elif p.val > root.val and q.val > root.val:
+            return self.lowestCommonAncestor(root.right, p, q)
+        return root
