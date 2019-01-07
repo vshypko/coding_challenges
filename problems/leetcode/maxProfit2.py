0a1f82d14c50c377a11926f795af4e003dbf1756
vitali <vshypko@gmail.com>
2019-01-06 23:56:37 -0800

    add maxProfit2

---
+++ problems/leetcode/maxProfit2.py
@@ -0,0 +1,17 @@
+# 122
+class Solution:
+    def maxProfit(self, prices):
+        """
+        :type prices: List[int]
+        :rtype: int
+        """
+        if not prices:
+            return 0
+
+        profit = 0
+
+        for i in range(len(prices) - 1):
+            if prices[i] < prices[i + 1]:
+                profit += prices[i + 1] - prices[i]
+
+        return profit
