743e047aabfe967929470f7ac2a5aa469dc3d228
vitali <vshypko@gmail.com>
2019-01-08 22:19:44 -0800

    update isBalanced

--- problems/ctci/chapter4/isBalanced.py
+++ problems/ctci/chapter4/isBalanced.py
@@ -6,39 +6,36 @@ class Tree(object):
         self.data = data
 
 
-class Solution:
-    def __init__(self):
-        # for testing purposes
-        pass
+# O(N) runtime
+# O(height(tree)) space
+def isBalanced(tree):
+    return checkHeight(tree) != float('-inf')
 
-    # O(N) runtime
-    # O(height(tree)) space
-    def isBalanced(self, tree):
-        return self.checkHeight(tree) != float('-inf')
 
-    def checkHeight(self, tree):
-        if not tree:
-            return -1
+def checkHeight(tree):
+    if not tree:
+        return -1
 
-        leftHeight = self.checkHeight(tree.left)
-        rightHeight = self.checkHeight(tree.right)
+    leftHeight = checkHeight(tree.left)
+    rightHeight = checkHeight(tree.right)
 
-        if leftHeight == float('-inf'):
-            return float('-inf')
+    if leftHeight == float('-inf'):
+        return float('-inf')
 
-        if rightHeight == float('-inf'):
-            return float('-inf')
+    if rightHeight == float('-inf'):
+        return float('-inf')
 
-        heightDiff = abs(leftHeight - rightHeight)
+    heightDiff = abs(leftHeight - rightHeight)
 
-        if heightDiff > 1:
-            return float('-inf')
-        else:
-            return max(leftHeight, rightHeight) + 1
+    if heightDiff > 1:
+        return float('-inf')
+    else:
+        return max(leftHeight, rightHeight) + 1
+
 
 tree = Tree(5)
 tree.left = Tree(3)
 tree.right = Tree(7)
 tree.right.right = Tree(7)
 # tree.right.right.right = Tree(7)
-print(Solution().isBalanced(tree))
+print(isBalanced(tree))
