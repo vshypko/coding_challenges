107200acd2bd1fc550102c30956c486c2b2dd5bb
vitali <vshypko@gmail.com>
2019-01-08 21:39:02 -0800

    add listOfDepths

--- problems/ctci/chapter4/listOfDepths.py
+++ problems/ctci/chapter4/listOfDepths.py
@@ -5,21 +5,40 @@ class Tree(object):
         self.right = None
         self.data = data
 
+# O(N) runtime
+# O(N) space
+def listOfDepths(root):
+    if not root:
+        return list()
 
-class Solution:
-    def __init__(self):
-        # for testing purposes
-        pass
+    result = list()
+    currentLevel = list()
+    currentLevel.append(root)
 
-    # O() runtime
-    # O() space
-    def listOfDepths(self, tree):
-        if not tree:
-            return 0
+    while currentLevel:
+        result.append(currentLevel)
+        parents = currentLevel
+        currentLevel = list()
 
+        for parent in parents:
+            if parent.left:
+                currentLevel.append(parent.left)
+            if parent.right:
+                currentLevel.append(parent.right)
+
+    return result
 
 
 tree = Tree(5)
 tree.left = Tree(3)
 tree.right = Tree(7)
-print(Solution().listOfDepths(tree))
+tree.left.left = Tree(8)
+tree.left.right = Tree(11)
+tree.right.left = Tree(13)
+
+# print(listOfDepths(tree))
+res = listOfDepths(tree)
+for i in res:
+    for j in i:
+        print(j.data, end=" ")
+    print()
